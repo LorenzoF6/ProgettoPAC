887c69b18228c4d4589ac11468ca9c1e692cc189
LorenzoF6 <lorenzo05698@gmail.com>
2024-05-02 18:09:05 +0200

    scheletro iniziale dell'applicazione spring boot

---
+++ Emergency_App/Emergency_App/src/main/java/com/emergency/Emergency_App/EmergencyAppApplication.java
@@ -0,0 +1,13 @@
+package com.emergency.Emergency_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyAppApplication.class, args);
+	}
+
+}
---
+++ Emergency_App/Emergency_App/src/test/java/com/emergency/Emergency_App/EmergencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emergency.Emergency_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
