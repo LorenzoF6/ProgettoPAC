d94715446068d6569cf49456e747d7697e27b5b9
LorenzoF6 <114820130+LorenzoF6@users.noreply.github.com>
2024-05-04 15:43:58 +0200

    Update Example_data.sql

--- Documentazione/Iterazione 0/Scripts/Example_data.sql
+++ Documentazione/Iterazione 0/Scripts/Example_data.sql
@@ -1,11 +1,11 @@
-INSERT INTO aat (id, nome, latitudine, longitudine) VALUES
+INSERT INTO aat (id_aat,nome_aat, latitudine_aat, longitudine_aat) VALUES
 (1,'BERGAMO',45.68575,9.63548),
 (2,'BRESCIA',45.55725,10.23114),
 (3,'SONDRIO',46.17100,9.88000),
 (4,'CREMONA',45.12967,10.05240),
 (5,'MONZA BRIANZA',45.60075,9.26030);
 
-INSERT INTO operatore118 (user, pwd, nome, cognome) VALUES
+INSERT INTO operatore118 (user_operatore118, pwd_operatore118, nome_operatore118, cognome_operatore118) VALUES
 (11801,'potHos','MARCO','ESPOSITO'),
 (11802,'albuca','LUCA','LEONE'),
 (11803,'macodes','ANNA','MONTI'),
@@ -13,7 +13,7 @@ INSERT INTO operatore118 (user, pwd, nome, cognome) VALUES
 (11805,'acero','AURORA','TORRE'),
 (11806,'bacillo','VALENTINA','FUMAGALLI');
 
-INSERT INTO ospedale (id, nome, latitudine, longitudine, disp) VALUES
+INSERT INTO ospedale (id_ospedale, nome_ospedale, latitudine_ospedale, longitudine_ospedale, disponibilita_ospedale) VALUES
 (1,'ROMANO DI LOMBARDIA',45.51936,9.75138,0),
 (2,'BERGAMO OVEST TREVIGLIO',45.50981,9.61150,1),
 (3,'SPEDALI CIVILI BRESCIA',45.55725,10.23114,1),
@@ -26,7 +26,7 @@ INSERT INTO ospedale (id, nome, latitudine, longitudine, disp) VALUES
 (10,'BERGAMO PAPA GIOVANNI',45.68575,9.63548,1);
 
 
-INSERT INTO mezzo (id, targa, tipo, stato, idAATM) VALUES
+INSERT INTO mezzo (id_mezzo, targa_mezzo, tipo_mezzo, stato_mezzo, id_aatm) VALUES
 (1,'CRI910AF','AMBULANZA','L',1),
 (2,'DY843XR','AMBULANZA','L',1),
 (3,'CRI12202','AMBULANZA','O',2),
@@ -41,7 +41,7 @@ INSERT INTO mezzo (id, targa, tipo, stato, idAATM) VALUES
 (12,'BR234TF','AMBULANZA','L',5);
 
 
-INSERT INTO squadra (id, stato_attuale, disp, idMezzo, idAATS) VALUES
+INSERT INTO squadra (id_squadra, stato_squadra, disponibilita_squadra, id_mezzos, id_aats) VALUES
 (1,'IN SEDE',0,1,1),
 (2,'IN SEDE',0,2,1),
 (3,'SUL POSTO',1,3,2),
@@ -54,7 +54,7 @@ INSERT INTO squadra (id, stato_attuale, disp, idMezzo, idAATS) VALUES
 (10,'RIENTRO IN SEDE',0,12,5);
 
 
-INSERT INTO volontario (id, nome, cognome, user, pwd, idSquadraV) VALUES
+INSERT INTO volontario (id_volontario, nome_volontario, cognome_volontario, user_volontario, pwd_volontario, id_squadrav) VALUES
 (1,'MARIO','RUSSO','V001','gelso',7),
 (2,'ROBERTO','BIANCHI','V002','prezzemolo',1),
 (3,'MASSIMO','RICCI','V003','vischio',6),
@@ -81,12 +81,12 @@ INSERT INTO volontario (id, nome, cognome, user, pwd, idSquadraV) VALUES
 (24,'MATTEO','VILLA','V024','dalia',3),
 (25,'GABRIELE','DE SANTIS','V025','calla',5);
 
-INSERT INTO emergenza (numero_missione, latitudine, longitudine, data, orario, motivo, codGravita, idOp118, idOspedale, idSquadraE) VALUES
+INSERT INTO emergenza (id_emergenza, latitudine_emergenza, longitudine_emergenza, data_emergenza, orario_emergenza, motivo_emergenza, codGravita_emergenza, id_operatore118e, id_ospedalee, id_squadrae) VALUES
 (1,45.54720,9.64294,'2023-12-16','23:35','INCIDENTE STRADALE','G',11803,2,5),
 (2,45.41631,9.73958,'2023-11-29','10:50','DOMESTICO','V',11805,4,8);
 
 
-INSERT INTO paziente (id,nome,cognome,eta,sesso,cosciente,respira,patologie,idEmergenza) VALUES
+INSERT INTO paziente (id_paziente,nome_paziente,cognome_paziente,eta_paziente,sesso_paziente,cosciente_paziente,respira_paziente,patologie_paziente,id_emergenzap) VALUES
 (1,'GIANCARLO','FAVRET',26,'M',1,1,'NO',1),
 (2,'GIACOMO','BONIZZI',46,'M',0,1,'NO',1),
 (3,'ELVIRA','GATTI',76,'F',1,1,'DIABETE',2);
